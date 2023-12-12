9949c8b3d108f0b6d6965c9d1b8d40d9c689ef68
codewithvishnu6 <152057125+codewithvishnu6@users.noreply.github.com>
2023-12-12 10:08:16 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// const main = async () => {
+// setInterval (function() {
+//     let a = f.gethours();
+//     let b = f.getMinutes();
+//     let c = f.getSeconds();
+
+//     document.getElementById("time").innerHTML = a + ":" + b + ":" + c + f;
+// }, 1000)
+// }
+// main()
